325504c34e436ee269fa280c1cec8f0272f50c27
lxy <1191512474>
2022-05-26 15:27:01 +0800

    four

--- app/src/main/java/com/example/temp/Test.java
+++
@@ -1,9 +0,0 @@
-package com.example.temp;
-
-import android.util.Log;
-
-public class Test {
-    public void temp(String str){
-        Log.e("qqq",str);
-    }
-}
--- plugin/src/main/java/com/lxy/plugins/repair/Test.java
+++
@@ -1,7 +0,0 @@
-package com.lxy.plugins.repair;
-
-public class Test {
-    public void test(){
-        System.out.println("123456");
-    }
-}
